f8e8b0df9509423a7b1eef6c2e86f920dcca2597
DESKTOP-VCURRJC\haita <h.aferyad.nawra@gmail.com>
2022-10-19 13:00:31 +0200

    add trigger to pipeline

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,6 +1,8 @@
 pipeline {
     agent any
 
+    triggers { pollSCM('* * * * *') }
+
     stages {
         stage('checkout'){
             steps {
@@ -14,8 +16,6 @@ pipeline {
             }
 
             post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
                 always {
                     junit '*target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
